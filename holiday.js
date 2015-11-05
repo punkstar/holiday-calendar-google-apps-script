f8848ccd4fb32b5c20797611ce7e736156df980c
Nick <nick@nicksays.co.uk>
2015-11-05 15:06:48 +0000

    Update holiday.js

--- holiday.js
+++ holiday.js
@@ -1,15 +1,25 @@
 /**
- * Pull all holidays out of the Holiday Calendar and insert it into a raw data spreadsheet.
+ * Pull all holidays out of the Meanbee Holiday Calendar and insert it into a raw data spreadsheet.
  */
 function fetch_holidays(){
-  var meanbee_holiday_calendar = 'yourcalendarurl@group.calendar.google.com';
-  var meanbee_holiday_spreadsheet_id = 'yourspreadsheetid';
+  var meanbee_holiday_calendar = 'calendarname@group.calendar.google.com';
+  var meanbee_holiday_spreadsheet_id = 'your_spreadsheet_id';
   var meanbee_holiday_sheet_name = 'Raw Holiday Data';
   
   var cal = CalendarApp.getCalendarById(meanbee_holiday_calendar);
   var sheet = SpreadsheetApp.openById(meanbee_holiday_spreadsheet_id).getSheetByName(meanbee_holiday_sheet_name);
    
   var events = cal.getEvents(new Date("January 1, 2000"), new Date("December 31, 3000"));
+
+  // 1 indexed, need the header row for ourselves.  
+  var row_number = 1;
+  
+  sheet.clearContents();
+  
+  var header_data = ['Person', 'Description', 'Working Days', 'Event Start Date', 'Event End Date', 'Is Half Day'];
+  var header_range = sheet.getRange(row_number++, 1, 1, header_data.length);
+  
+  header_range.setValues([header_data]);
   
   for (var i = 0; i < events.length; i++) {
     
@@ -20,10 +30,14 @@ function fetch_holidays(){
     // Call the accessors again to get a fresh object
     var event_duration_working_days = working_days_in_range(events[i].getStartTime(), events[i].getEndTime());
     
-    var row_number = i + 2; // Because we want the header row for ourselves
-    
     var staff_member = event_title.split(/:/)[0];
     var is_half_day  = !!event_title.match(/half day/i);
+    var is_holiday   = !!event_title.match(/holiday/i);
+    
+    
+    if (!is_holiday) {
+      continue; 
+    }
     
     if (is_half_day) {
       event_duration_working_days = 0.5;
@@ -38,7 +52,7 @@ function fetch_holidays(){
       is_half_day
     ];
     
-    var range = sheet.getRange(row_number, 1, 1, row_data.length);
+    var range = sheet.getRange(row_number++, 1, 1, row_data.length);
     
     range.setValues([row_data]);
   }
