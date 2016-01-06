d7d30e39343635e2505d4ae3d1983a271b608a9b
Nick <nick@nicksays.co.uk>
2016-01-06 09:36:48 +0000

    Split events spanning multiple days into individual days
    This makes it easier to attribute holidays to months and makes the logic around the calculation of working days simpler.

--- holiday.js
+++ holiday.js
@@ -1,7 +1,7 @@
 /**
  * Pull all holidays out of the Meanbee Holiday Calendar and insert it into a raw data spreadsheet.
  */
-function fetch_holidays(){
+function fetch_holidays_v2() {
   var meanbee_holiday_calendar = 'calendarname@group.calendar.google.com';
   var meanbee_holiday_spreadsheet_id = 'your_spreadsheet_id';
   var meanbee_holiday_sheet_name = 'Raw Holiday Data';
@@ -23,57 +23,56 @@ function fetch_holidays(){
   
   for (var i = 0; i < events.length; i++) {
     
-    var event_title         = events[i].getTitle();
-    var event_start_day     = events[i].getStartTime();
-    var event_end_day       = events[i].getEndTime();
+    var event = events[i];
     
-    // Call the accessors again to get a fresh object
-    var event_duration_working_days = working_days_in_range(events[i].getStartTime(), events[i].getEndTime());
+    var event_title         = event.getTitle();
+    var event_start_day     = event.getStartTime();
+    var event_end_day       = event.getEndTime();
     
-    var staff_member = event_title.split(/:/)[0];
-    var is_half_day  = !!event_title.match(/half day/i);
-    var is_holiday   = !!event_title.match(/holiday/i);
+    var date_current_day    = event_start_day;
     
-    
-    if (!is_holiday) {
-      continue; 
+    /* Loop through each day in the event and create a new row in the spreadsheet for it */ 
+    while (date_current_day < event_end_day) {
+      var staff_member = event_title.split(/:/)[0];
+      var is_half_day  = is_event_half_day(event, date_current_day);
+      var is_working_day = is_date_working_day(date_current_day);
+      var is_holiday   = is_event_holiday(event);
+      
+      if (is_holiday && is_working_day) {
+        var event_duration_working_days = (is_half_day) ? 0.5 : 1;
+        
+        var row_data = [
+          staff_member,
+          event_title,
+          event_duration_working_days,
+          date_current_day,
+          date_current_day,
+          is_half_day
+        ];
+        
+        var range = sheet.getRange(row_number++, 1, 1, row_data.length);
+        
+        range.setValues([row_data]);
+      }
+      
+      date_current_day.setDate(date_current_day.getDate() + 1);
     }
-    
-    if (is_half_day) {
-      event_duration_working_days = 0.5;
-    }
-    
-    var row_data = [
-      staff_member,
-      event_title,
-      event_duration_working_days,
-      event_start_day,
-      event_end_day,
-      is_half_day
-    ];
-    
-    var range = sheet.getRange(row_number++, 1, 1, row_data.length);
-    
-    range.setValues([row_data]);
   }
 }
 
-function working_days_in_range(start_range, end_range) {
-  var day_in_ms = 24 * 60 * 60 * 1000;
-  var current_day = start_range;
-  var working_days = 0;
+function is_event_holiday(event) {
+  return !!event.getTitle().match(/holiday/i);
+}
+
+function is_event_half_day(event, event_start_time) {
+  var half_day_in_title = !!event.getTitle().match(/half day/i);
+  var is_friday         = event_start_time.getDay() == 5;
   
-  while (current_day < end_range) {
-    var day_of_week = current_day.getDay();
-    
-    if (day_of_week >= 1 && day_of_week <= 4) {
-      working_days += 1.0;
-    } else if (day_of_week == 5) {
-      working_days += 0.5; 
-    }
-    
-    current_day.setDate(current_day.getDate() + 1);
-  }
+  return half_day_in_title || is_friday;
+}
+
+function is_date_working_day(date) {
+  var day = date.getDay();
   
-  return working_days;
+  return day >= 1 && day <= 5;
 }
